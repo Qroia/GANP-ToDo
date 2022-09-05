3f5b15fb0c4b570077f99d38a053ed6f7698c6fb
Qroia FAK(C)E <68863850+Qroia@users.noreply.github.com>
2022-09-05 10:05:12 +0300

    CORS Fix [Nginx]

--- front/src/app/graphql.module.ts
+++ front/src/app/graphql.module.ts
@@ -4,7 +4,7 @@ import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
 import { environment } from "../environments/environment";
 
-const uri = 'http://localhost:3001' + environment.graphql_uri;
+const uri = environment.graphql_uri;
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
